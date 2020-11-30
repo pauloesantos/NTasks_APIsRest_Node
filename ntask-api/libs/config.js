68d03bb4574c6bf2fe126941bb97c223686d6b45
Paulo <paulo.esantos@gmail.com>
2020-11-30 12:26:21 -0300

    final da APi

--- ntask-api/libs/config.js
+++ ntask-api/libs/config.js
@@ -1,15 +1,7 @@
-module.exports = {
-    database: "ntask",
-    username: "",
-    password: "",
-    params: {
-        dialect: "sqlite",
-        storage: "ntask.sqlite",
-        logging: console.log,
-        define: {
-            underscored: true
-        }
-    },
-    jwtSecret: "Ntas$k_AP1",
-    jwtSession: { session: false }
+module.exports = app => {
+  const env = process.env.NODE_ENV;
+  if (env) {
+    return require(`./config.${env}.js`);
+  }
+  return require("./config.development.js");
 };
